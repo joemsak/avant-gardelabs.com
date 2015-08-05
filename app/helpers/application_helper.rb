4025eb262e7f8c5209c3c4c7ed44bece5702986d
Joe Sak <joe@joesak.com>
2015-08-05 08:47:07 -0500

    Add the browser title helper and a little flare to the first modern bullet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def browser_title
+    base_title = 'Avant-Garde • Modern Software Development'
+
+    if content_for?(:title)
+      "#{content_for(:title)} • #{base_title}"
+    else
+      base_title
+    end
+  end
 end
